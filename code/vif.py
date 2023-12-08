0f144b1ac0b5b7db4e4cda2f28fd0382caac4a56
Suze Frikkee <129743493+suus-stack@users.noreply.github.com>
2023-12-08 11:01:00 +0100

    correct vif

--- code/vif.py
+++ code/vif.py
@@ -1,20 +1,31 @@
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 
+chosen_countries = ['Australia', 'Austria', 'Belgium', 'Canada', 'Chile', 'Czechia', 
+                    'Estonia', 'Finland', 'Germany', 'Greece', 'Hungary', 'Iceland', 
+                    'Ireland', 'Italy', 'Korea', 'Latvia', 'Luxembourg', 'Netherlands', 
+                    'New Zealand', 'Norway', 'Portugal', 'Slovak Republic', 'Slovenia', 
+                    'Spain', 'Sweden', 'Türkiye']
+
 pharma_sales = pd.read_csv('../data/pharma_sales_ppp.csv')
 pharma_sales_2014 = pharma_sales[pharma_sales['Year'] == 2014]
 nervous = 'N-Nervous system' 
 anti_dep = 'N06A-Antidepressants'
 ulcer = 'A02B-Drugs for peptic ulcer and gastro-oesophageal reflux diseases (GORD)'
 
-# All independent variables data for 2014
-nervous_data = pharma_sales_2014[pharma_sales_2014['Variable'] == nervous]['Value'][0:28].reset_index(drop=True)
-anti_depressant_data = pharma_sales_2014[pharma_sales_2014['Variable'] == anti_dep]['Value'].reset_index(drop=True)
-ulcer_data = pharma_sales_2014[pharma_sales_2014['Variable'] == ulcer]['Value'].reset_index(drop=True)
+# All independent variables data for 2014 and the chosen countries
+nervous_data = pharma_sales_2014[pharma_sales_2014['Variable'] == nervous][['Value', 'Country']].reset_index(drop=True)
+nervous_data_filtered = nervous_data[nervous_data['Country'].isin(chosen_countries)].reset_index(drop=True)
+
+anti_depressant_data = pharma_sales_2014[pharma_sales_2014['Variable'] == anti_dep][['Value', 'Country']].reset_index(drop=True)
+anti_depressant_data_filtered = anti_depressant_data[anti_depressant_data['Country'].isin(chosen_countries)].reset_index(drop=True)
+
+ulcer_data = pharma_sales_2014[pharma_sales_2014['Variable'] == ulcer][['Value', 'Country']].reset_index(drop=True)
+ulcer_data_filtered = ulcer_data[ulcer_data['Country'].isin(chosen_countries)].reset_index(drop=True)
 
 # Creating dataframe of all independent values data
 column_names = ['N-nervous system', 'Anti_depressant', 'Peptic ulcer']
-data = {column_names[0]: nervous_data, column_names[1]: anti_depressant_data,column_names[2]: ulcer_data}
+data = {column_names[0]: nervous_data_filtered['Value'], column_names[1]: anti_depressant_data_filtered['Value'], column_names[2]: ulcer_data_filtered['Value']}
 
 X = pd.DataFrame(data, columns=column_names)
 print(X)
