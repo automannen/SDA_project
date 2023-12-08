829ca9e3286f2ccf408ce7bc66713abd841a0879
Suze Frikkee <129743493+suus-stack@users.noreply.github.com>
2023-12-08 10:35:25 +0100

    updated vif

--- code/vif.py
+++ code/vif.py
@@ -2,25 +2,22 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 pharma_sales = pd.read_csv('../data/pharma_sales_ppp.csv')
-
-R03 = 'R03-Drugs for obstructive airway diseases'
+pharma_sales_2014 = pharma_sales[pharma_sales['Year'] == 2014]
+nervous = 'N-Nervous system' 
 anti_dep = 'N06A-Antidepressants'
 ulcer = 'A02B-Drugs for peptic ulcer and gastro-oesophageal reflux diseases (GORD)'
 
-# All independent variables 
-R03_data = pharma_sales[pharma_sales['Variable'] == R03]['Value'].reset_index(drop=True)
-anti_depressant_data = pharma_sales[pharma_sales['Variable'] == anti_dep]['Value'].reset_index(drop=True)
-ulcer_data = pharma_sales[pharma_sales['Variable'] == ulcer]['Value'].reset_index(drop=True)
+# All independent variables data for 2014
+nervous_data = pharma_sales_2014[pharma_sales_2014['Variable'] == nervous]['Value'][0:28].reset_index(drop=True)
+anti_depressant_data = pharma_sales_2014[pharma_sales_2014['Variable'] == anti_dep]['Value'].reset_index(drop=True)
+ulcer_data = pharma_sales_2014[pharma_sales_2014['Variable'] == ulcer]['Value'].reset_index(drop=True)
 
-# Creating dataframe of all indpendent values data
-column_names = ['R03', 'Anti_depressant', 'Peptic ulcer']
-data = {
-    'R03': R03_data,
-    'Anti_depressant': anti_depressant_data,
-    'Peptic ulcer': ulcer_data
-}
+# Creating dataframe of all independent values data
+column_names = ['N-nervous system', 'Anti_depressant', 'Peptic ulcer']
+data = {column_names[0]: nervous_data, column_names[1]: anti_depressant_data,column_names[2]: ulcer_data}
 
 X = pd.DataFrame(data, columns=column_names)
+print(X)
 
 for i, col in enumerate(X.columns):
     y = X[col]
