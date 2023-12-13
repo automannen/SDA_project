088d8f69d4915e759f77e1a7700a178e219f5008
heckergorl <chezan.carmen@gmail.com>
2023-12-13 17:02:24 +0100

    added the missingness feature

--- code/combinations_regression.py
+++ code/combinations_regression.py
@@ -9,13 +9,14 @@ from tqdm import tqdm
 
 merged_df = pd.read_csv('merged_data.csv')
 
-n_variables = 5
+n_variables = 10
 drug_vars = merged_df['Pharma_Sales_Variable'].unique().tolist()
 drug_combis = list(combinations(drug_vars, n_variables))
 
 df = defaultdict(lambda: defaultdict(lambda: []))
-for drug in tqdm(merged_df[["Pharma_Sales_Variable", "Pharma_Sales_Value", "Life_Expectancy_Variable"]].iterrows()):
+for drug in tqdm(merged_df[["Pharma_Sales_Variable", "Pharma_Sales_Value", "Life_Expectancy_Variable", "Missingness_Indicator"]].iterrows()):
     df[drug[1]["Life_Expectancy_Variable"]][drug[1]["Pharma_Sales_Variable"]].append(drug[1]["Pharma_Sales_Value"])
+    df[drug[1]["Life_Expectancy_Variable"]][drug[1]["Pharma_Sales_Variable"]+"missingness"].append(drug[1]["Missingness_Indicator"])
 
 df = pd.DataFrame(df)
 # print(df)
@@ -25,29 +26,28 @@ df = pd.DataFrame(df)
 
 
 # Run generalized linear models for each life expectancy variable
-for life_exp_variable in merged_df['Life_Expectancy_Variable'].unique():
+for life_exp_variable in tqdm(merged_df['Life_Expectancy_Variable'].unique()):
 
 #   print(f"\nResults for Life Expectancy: {life_exp_variable}")
   current_life_exp_df = merged_df[merged_df['Life_Expectancy_Variable'] == life_exp_variable]
 
   for pharma_sales_variables in drug_combis:
     # print(f"Results for Pharma Sales: {pharma_sales_variables}")
-    X = np.zeros((1650, n_variables))
+    X = np.zeros((1650, n_variables*2))
 
-    # Filter the DataFrame to include only the current life expectancy variable and pharma sales variable
+    # filling the X matrix
     for i, drug in enumerate(pharma_sales_variables):
         X[:, i] = np.array(df[life_exp_variable][drug])
-    # print(current_life_exp_df)
-    # print(list(pharma_sales_variables))
+        X[:, i + n_variables] = np.array(df[life_exp_variable][drug+"missingness"])
 
     y = current_life_exp_df[(current_life_exp_df["Pharma_Sales_Variable"] == list(pharma_sales_variables)[0])]
     y = y["Life_Expectancy_Value"]
-    # print(np.shape(current_life_exp_df[["Life_Expectancy_Value"]]))
 
     model = LinearRegression()
     model.fit(X, y)
     score = model.score(X, y)
-    if score > 0.5:
+    # threshold for the score
+    if score > 0.6:
         print(f"Results for Pharma Sales: {pharma_sales_variables}")
         print(f"\nResults for Life Expectancy: {life_exp_variable}")
         print("LETSGO", score)
