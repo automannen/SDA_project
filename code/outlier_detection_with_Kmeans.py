57a21cfd656880db21132dce99d02f804d48e068
“Olivier <Olivierguntenaar@gmail.com>
2023-12-20 15:55:09 +0100

    outlier_detection clean up

--- code/outlier_detection_with_Kmeans.py
+++ code/outlier_detection_with_Kmeans.py
@@ -5,85 +5,61 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import os
 
-# TODO: hier staat nog de KMeans clustering in en die hebben we nodig voor onze progressie plot. die er tot had geleid dat we Nearest Neighbors hebben gebruikt.
-
+# Read the merged data
 merged_df = pd.read_csv('merged_data.csv')
-drugs = merged_df['Pharma_Sales_Variable'].unique()[1:]# skipping the total sales variable
+drugs = merged_df['Pharma_Sales_Variable'].unique()[1:]  # Excluding the total sales variable
 countries = merged_df['Country'].unique()
 
-
-# Directory for saving plots
+# Create directory for saving plots if it doesn't exist
 plots_directory = '../data_visualization/clustering'
 os.makedirs(plots_directory, exist_ok=True)
 
-
-# Parameters
+# Parameters for PCA and KMeans
 n_components = 2  # Number of PCA components
 n_clusters = 4  # Number of clusters
 
-
-# Function to perform PCA, KMeans and identify single-item clusters
 def identify_single_item_clusters(X, n_components, n_clusters):
-    # Applying PCA
+    """Performs PCA and KMeans clustering to identify single-item clusters."""
+    # Apply PCA
     pca = PCA(n_components)
     X_pca = pca.fit_transform(X)
 
-    # KMeans clustering
-    kmeans = KMeans(n_clusters=n_clusters)
+    # Perform KMeans clustering
+    kmeans = KMeans(n_clusters=n_clusters, n_init='auto')
     labels = kmeans.fit_predict(X_pca)
 
-    # Identifying clusters with only one item
+    # Identify clusters with only one item
     unique, counts = np.unique(labels, return_counts=True)
     single_item_clusters = unique[counts == 1]
-
-    # Finding the indices of these single-item clusters
     single_item_indices = np.where(np.isin(labels, single_item_clusters))[0]
 
     return single_item_indices, X_pca, labels
 
-
-# Analyzing for each gender
 single_item_clusters_info = {}
 
-
 for gender in merged_df['Life_Expectancy_Variable'].unique():
+    # Create an array to store data for clustering
     X = np.zeros((len(countries), len(drugs)))
-
     for i, drug in enumerate(drugs):
-        # print(i, drug)
-        info_drug_x = merged_df[merged_df['Pharma_Sales_Variable'] == drug][['Pharma_Sales_Variable', 'Pharma_Sales_Value', 'Life_Expectancy_Value', 'Life_Expectancy_Variable']]
-        filter_gender = info_drug_x[info_drug_x['Life_Expectancy_Variable'] == gender]
+        filtered_data = merged_df[(merged_df['Pharma_Sales_Variable'] == drug) &
+                                  (merged_df['Life_Expectancy_Variable'] == gender)]
+        X[:, i] = filtered_data['Pharma_Sales_Value'].values
 
-        x = np.array(filter_gender['Pharma_Sales_Value'])
-        X[:, i] = x
-
-    # Identifying single-item clusters
+    # Perform clustering
     single_item_indices, X_pca, labels = identify_single_item_clusters(X, n_components, n_clusters)
     single_item_clusters_info[gender] = single_item_indices
 
-    print(gender)
+    plt.figure()
+    for cluster_id in np.unique(labels):
+        plt.scatter(X_pca[labels == cluster_id, 0], X_pca[labels == cluster_id, 1], label=cluster_id)
 
-    # scatterplot all dots without single_item_indices_nn
-    for i in range(len(X_pca)):
-        if i not in single_item_indices:
-            plt.scatter(X_pca[i, 0], X_pca[i, 1], label=i, color='blue')
-        else:
-            plt.scatter(X_pca[i, 0], X_pca[i, 1], label=i, color='red')
-
-
-    # Some combination of drug-related features that contributed most to the variability in the data
     plt.xlabel('First principal component values')
-    # Another composite measure/combination of features, capturing remaining variability
     plt.ylabel('Second principal component values')
-
     plt.legend()
-    plt.title('Outliers KMeans ' + gender)
+    plt.title(f'Outliers KMeans {gender}')
 
-    plt_name = 'KNN_' + str(gender.replace(' ', '_')) + '.png'
+    plt_name = f'KMeans_{gender.replace(" ", "_")}.png'
     heatmap_path = os.path.join(plots_directory, plt_name)
     plt.savefig(heatmap_path)
     plt.close()
-
-
-
 
